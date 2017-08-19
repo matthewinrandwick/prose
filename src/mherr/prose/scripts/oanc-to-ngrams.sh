a9edb43f2f58ba9335ce40df316847c4cd385871
Matthew Herrmann <matthewinrandwick@gmail.com>
2017-08-19 13:07:46 +1000

    parameterize oanc-to-ngrams script

--- src/mherr/prose/scripts/oanc-to-ngrams.sh
+++ src/mherr/prose/scripts/oanc-to-ngrams.sh
@@ -2,9 +2,17 @@
 
 set -o errexit
 
+if [[ $1 == "" ]]; then
+  echo "Usage: $(basename $0) [oanc-dir]
+
+  Converts the unzipped OANC corpus into ngrams.
+  " >&2
+  exit 1
+fi
+corpus="$1"
 path=$(dirname $0)
 cd "$path/.."
-files=$(find sources/OANC-GrAF -name "*.txt")
+files=$(find "$corpus" -name "*.txt")
 
 for n in $(seq 2 5); do
   go run cmd/ngrams-from-text/main.go --filter $n $files > ngrams.$n.txt
